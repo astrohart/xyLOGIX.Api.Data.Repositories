e7141dbf29defdae02ab00fbfea2c164d805cf8e
Brian Hart <brian.hart@xylogix.net>
2023-09-14 23:53:31 -0600

    Latest updates at 23:52 hours MDT on 09/14/2023

--- xyLOGIX.Api.Data.Repositories.Events/IterationErrorEventArgs.cs
+++ xyLOGIX.Api.Data.Repositories.Events/IterationErrorEventArgs.cs
@@ -1,38 +1,19 @@
-using System;
+﻿using System;
 using xyLOGIX.Api.Data.Iterators.Exceptions;
 
 namespace xyLOGIX.Api.Data.Repositories.Events
 {
-   /// <summary>
-   /// Defines the data that is passed by all events of type
-   /// <see cref="T:xyLOGIX.Api.Data.Iterators.Events.IterationErrorEventHandler" />.
-   /// </summary>
+///  <summary> Defines the data that is passed by all events of type <see cref="T:xyLOGIX.Api.Data.Iterators.Events.IterationErrorEventHandler" />. </summary>
    public class IterationErrorEventArgs : EventArgs
    {
-      /// <summary>
-      /// Creates a new instance of
-      /// <see
-      ///    cref="T:xyLOGIX.Api.Data.Repositories.Events.IterationErrorEventArgs" />
-      /// and
-      /// returns a reference to it.
-      /// </summary>
-      /// <param name="exception">
-      /// (Required.) A <see cref="T:xyLOGIX.Api.Data.Iterators.Exceptions.IteratorException" /> that describes the error.
-      /// </param>
-      /// <exception cref="T:System.ArgumentNullException">
-      /// Thrown if the required parameter, <paramref name="exception" />, is
-      /// <c>null</c> .
-      /// </exception>
+///  <summary> Creates a new instance of <see cref="T:xyLOGIX.Api.Data.Repositories.Events.IterationErrorEventArgs" /> and returns a reference to it. </summary> <param name="exception"> (Required.) A <see cref="T:xyLOGIX.Api.Data.Iterators.Exceptions.IteratorException" /> that describes the error. </param> <exception cref="T:System.ArgumentNullException"> Thrown if the required parameter, <paramref name="exception" />, is <c>null</c> . </exception>
       public IterationErrorEventArgs(IteratorException exception)
       {
          Exception = exception ??
                      throw new ArgumentNullException(nameof(exception));
       }
 
-      /// <summary>
-      /// Gets a reference to a <see cref="T:xyLOGIX.Api.Data.Iterators.Exceptions.IteratorException" /> that contains
-      /// the error information.
-      /// </summary>
+///  <summary> Gets a reference to a <see cref="T:xyLOGIX.Api.Data.Iterators.Exceptions.IteratorException" /> that contains the error information. </summary>
       public IteratorException Exception { get; }
    }
 }
