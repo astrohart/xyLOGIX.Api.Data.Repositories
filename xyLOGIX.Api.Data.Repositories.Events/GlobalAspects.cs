17476be1cc5e4ba041318732c767b9bfbc6f9269
Brian Hart <brian.hart@xylogix.net>
2023-11-01 17:34:41 -0600

    Latest updates at 17:34 hours MDT on 11/01/2023

--- xyLOGIX.Api.Data.Repositories.Events/GlobalAspects.cs
+++ xyLOGIX.Api.Data.Repositories.Events/GlobalAspects.cs
@@ -1,4 +1,4 @@
-﻿using PostSharp.Extensibility;
+using PostSharp.Extensibility;
 using PostSharp.Patterns.Diagnostics;
 
 // This file contains registration of aspects that are applied to several classes of this project.
@@ -15,7 +15,8 @@ using PostSharp.Patterns.Diagnostics;
                                           MulticastAttributes.UserGenerated
     )]
 [assembly: Log(AttributePriority = 1)]
-[assembly: Log(AttributePriority = 2, AttributeExclude = true, AttributeTargetMembers = "get_*" )]
-[assembly: Log(AttributePriority = 3, AttributeExclude = true, AttributeTargetMembers = "set_*" )]
-[assembly: Log(AttributePriority = 4, AttributeExclude = true, AttributeTargetMembers = "add_*" )]
-[assembly: Log(AttributePriority = 5, AttributeExclude = true, AttributeTargetMembers = "remove_*" )]
+[assembly:
+    Log(
+        AttributeExclude = true, AttributeTargetMembers = "regex:^get_|^set_|^add_|^remove_",
+        AttributePriority = 2
+    )]
